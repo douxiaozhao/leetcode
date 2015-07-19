3e0347cf70c37485a95a1e51f08f1984b4b77948
dou <douxiaozhao@163.com>
2015-07-19 09:33:10 +0800

    矩阵旋转，每四个点是一个旋转环

---
+++ 48_RotateImage.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int> >& matrix) {
+        int n = matrix.size();
+        int limit = (n-1)/2;
+        for(int i=0;i<= limit; i++)
+        {
+            for(int j=i;j<n-1-i;j++)
+            {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n-1-j][i];
+                matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
+                matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+                matrix[j][n-1-i] = temp;
+            }
+        }
+    }
+};
